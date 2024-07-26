3d8aafa34b2a7f38e1da7b23f1edcc1fe083f986
Thomas Choquet <semoir.dense-0h@icloud.com>
2024-07-26 10:06:24 +0900

    ECS WIP

--- include/Game-Engine/ECSWorld.hpp
+++ include/Game-Engine/ECSWorld.hpp
@@ -12,19 +12,39 @@
 
 #include "UtilsCPP/Array.hpp"
 #include "UtilsCPP/Dictionary.hpp"
+#include "UtilsCPP/Func.hpp"
+#include "UtilsCPP/Set.hpp"
 #include "UtilsCPP/Types.hpp"
 #include "UtilsCPP/UniquePtr.hpp"
 
 namespace GE
 {
 
-using EntityID    = utils::uint64;
-using ComponentID = utils::uint32;
-
-class Archetype;
+using EntityID = utils::uint64;
 
 class ECSWorld
 {
+private:
+    using ComponentID = utils::uint32;
+    using ArchetypeID = utils::Set<ComponentID>;
+
+    struct Archetype
+    {
+        ArchetypeID id;
+        utils::Dictionary<ComponentID, void*> rows;
+        utils::Array<utils::uint32> availableIndices;
+        utils::Dictionary<ComponentID, Archetype*> edgeAdd;
+        utils::Dictionary<ComponentID, Archetype*> edgeRemove;
+
+        utils::uint32 makeRoom();
+    };
+
+    struct EntityData
+    {
+        Archetype* archetype = nullptr;
+        utils::uint32 idx = 0;
+    };
+
 public:
     ECSWorld()                = default;
     ECSWorld(const ECSWorld&) = delete;
@@ -33,37 +53,46 @@ public:
     EntityID createEntity();
     void deleteEntity(EntityID);
     
-    template<typename T> inline void addComponent(EntityID entity, const T& component) { addComponent(entity, componentID<T>(), sizeof(T), [](void* ptr){ ((T*)ptr)->~T(); }); }
-    template<typename T> void removeComponent(EntityID);
+    template<typename T>
+    void addComponent(EntityID entityID, const T& component)
+    {
+        EntityData& entity = m_entityDatas[entityID];
+
+        Archetype* newArchetype = archetypeEdgeAdd(entity.archetype, componentID<T>(), sizeof(T), utils::Func<void(void*)>([](void* ptr){ ((T*)ptr).~T(); }));
+        utils::uint32 newArchetypeIdx = newArchetype->makeRoom();
+        moveComponents(entity.archetype, entity.idx, newArchetype, newArchetypeIdx);
+
+        new (&newArchetype->rows[componentID<T>()][newArchetypeIdx]) T(component);
+    }
+
+    template<typename T>
+    void removeComponent(EntityID entityID)
+    {
+        EntityData& entity = m_entityDatas[entityID];
+
+        ((T*)(entity.archetype->rows[componentID<T>()][entity.idx])).~T();
+
+        Archetype* newArchetype = archetypeEdgeRemove(entity.archetype, componentID<T>());
+        utils::uint32 newArchetypeIdx = newArchetype->makeRoom();
+        moveComponents(entity.archetype, entity.idx, newArchetype, newArchetypeIdx);
+    }
 
     ~ECSWorld() = default;
 
 private:
-    struct EntityData
-    {
-        Archetype* archetype = nullptr;
-        utils::uint32 idx = 0;
-    };
+    template<typename T> inline ComponentID componentID() { static ComponentID id = m_nextComponentID++; return id; }
 
-    template<typename T>
-    ComponentID componentID()
-    {
-        static ComponentID id = m_nextComponentID++;
-        return id;
-    }
+    Archetype* archetypeEdgeAdd(Archetype*, ComponentID, utils::uint32 size, const utils::Func<void(void*)>& destructor);
+    Archetype* archetypeEdgeRemove(Archetype*, ComponentID);
 
-    void addComponent(EntityID, ComponentID, void* data, utils::uint32 size, utils::Func<void(void*)> destructor);
-    void removeComponent(EntityID, ComponentID);
-
-    Archetype* newArchetype();
+    void moveComponents(Archetype* src, utils::uint32 srcIdx, Archetype* dst, utils::uint32 dstIdx);
 
     ComponentID m_nextComponentID = 0;
 
     utils::Array<EntityData> m_entityDatas;
     utils::Array<EntityID> m_availableEntityIDs;
     
-    utils::Dictionary<ComponentID, utils::UniquePtr<Archetype>> m_rootArchetypes;
-    utils::Array<utils::UniquePtr<Archetype>> m_archetypes;
+    utils::Dictionary<ArchetypeID, utils::UniquePtr<Archetype>> m_archetypes;
     
 public:
     ECSWorld& operator = (const ECSWorld&) = delete;
