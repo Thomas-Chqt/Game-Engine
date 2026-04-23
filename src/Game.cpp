a7925a32720dedfa5e569187dcc2b2d5e07161d1
Thomas <semoir.dense-0h@icloud.com>
2026-04-24 07:42:11 +0900

    Use std::ranges::to for Game scene map construction

--- src/Game.cpp
+++ src/Game.cpp
@@ -71,10 +71,12 @@ Game::Game(
 )
     : m_makeScriptInstance(std::move(makeScriptInstance))
     , m_listScriptParameters(std::move(listScriptParameters))
+    , m_scenes(std::ranges::to<std::map<std::string, Scene>>(
+          descriptor.scenes | std::views::transform([&](const auto& sceneDesc) {
+              return std::make_pair(sceneDesc.first, Scene(assetManager, sceneDesc.second));
+          })))
     , m_inputContext(descriptor.inputContext)
 {
-    for (const auto& [name, sceneDescriptor] : descriptor.scenes)
-        m_scenes.emplace(name, Scene(assetManager, sceneDescriptor));
     setActiveScene(descriptor.activeScene);
 }
 
